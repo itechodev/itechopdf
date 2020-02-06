5bc7a8fabceb9cfe4b0ddd0bcf7b829491aee55c
Willem Bijker <willem@itecho.co.za>
2020-02-06 11:29:40 +0200

    Add script to inject params in headers / footers

--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@ namespace wkpdftoxcorelib
             var htmltopdf = new WkHtmlToPdf();
             Console.WriteLine("WkHTML version:" + htmltopdf.GetVersion());
             // htmltopdf.PrintSettings.Margins.Set(0, 0, 0, 0, Unit.Centimeters);
+
+            htmltopdf.PrintSettings.EnableJavascript = true;
             htmltopdf.PrintSettings.Margins.Unit = Unit.Millimeters;
             htmltopdf.PrintSettings.Margins.Left = 0;
             htmltopdf.PrintSettings.Margins.Right = 0;
@@ -23,16 +25,20 @@ namespace wkpdftoxcorelib
             htmltopdf.PrintSettings.Margins.Top = 0;
             htmltopdf.PrintSettings.Margins.Bottom = 0;
             
+            htmltopdf.PrintSettings.Orientation = Orientation.Landscape;
             htmltopdf.PrintSettings.Header = new FileHeaderFooter("header.html");
             htmltopdf.PrintSettings.Header.Height = 25;
             htmltopdf.PrintSettings.Header.Spacing = 10; 
 
             htmltopdf.PrintSettings.Footer = new FileHeaderFooter("header.html");
-            htmltopdf.PrintSettings.Footer.Spacing = 10; // 1 cm
             htmltopdf.PrintSettings.Footer.Height = 25; 
+            htmltopdf.PrintSettings.Footer.Spacing = 10; // 1 cm
 
-            var doc = htmltopdf.HtmlToPdf("<style>* {margin: 0; padding:0}</style> <body style='background-color: yellow; margin:0; padding:0;'>To PDF seems to be working just fine</div>");
+            var doc = htmltopdf.HtmlFileToPdf("content.html");
             doc?.SaveToFile("output.pdf");
+
+
+        }
         }
     }
 }
