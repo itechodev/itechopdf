402abaa86ff2e06d0408f1eeb0176eb59f774dec
Willem Bijker <willem@itecho.co.za>
2020-02-08 10:22:39 +0200

    Add console test app

---
+++ testconsole/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+using wkpdftoxcorelib;
+
+namespace testconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // No data is available for encoding 1252
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var renderer = new PdfRenderer();
+            Console.WriteLine("WkHTML version:" + renderer.GetVersion());
+
+            var cover = new PdfDocument();
+            // cover.Configure(print => {
+            //     print.Margins.Set(0, 0, 0, 0, Unit.Millimeters);
+            //     print.Orientation = Orientation.Landscape;
+            // });
+            cover.FromFile("res/cover.html");
+
+            var content = new PdfDocument();
+            // content.Configure(print => {
+            //     print.Margins.Set(0, 0, 0, 0, Unit.Millimeters);
+            //     print.Orientation = Orientation.Portrait;
+            // });
+            content.AddFileHeader("res/header.html", 25, 10);
+            content.AddFileFooter("res/footer.html", 25, 10);
+            content.FromFile("res/content.html");
+
+            renderer.Add(cover);
+            renderer.Add(content);
+
+            var pdf = renderer.RenderToBytes();
+            File.WriteAllBytes("output.pdf", pdf);
+        }
+    }
+}
+
