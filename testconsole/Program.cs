0bd54c983adb8361a04169c3294b0eab1e0ac0e0
Willem Bijker <willem@itecho.co.za>
2020-05-11 07:49:57 +0200

    Add pdfSharp lib and clipping

--- testconsole/Program.cs
+++ testconsole/Program.cs
@@ -5,6 +5,9 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using ItechoPdf;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
 
 namespace testconsole
 {
@@ -33,6 +36,24 @@ namespace testconsole
 
             return renderer.RenderToBytes();
         }
+
+        private static void PdfClipping()
+        {
+            // Open the external documents as XPdfForm objects. Such objects are
+            // treated like images. By default the first page of the document is
+            // referenced by a new XPdfForm.
+            XPdfForm form1 = XPdfForm.FromFile("input/b.pdf");
+
+            PdfSharp.Pdf.PdfDocument outputDocument = new PdfSharp.Pdf.PdfDocument();
+
+            PdfSharp.Pdf.PdfPage page1 = outputDocument.AddPage();
+
+            var gfx = XGraphics.FromPdfPage(page1);
+            gfx.DrawImage(form1, new XRect(0, 0, 100, 100));
+
+            outputDocument.Save("output/b.pdf");
+        }
+
 
         static void Main(string[] args)
         {
